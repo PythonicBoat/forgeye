014cda0540af39c661cf25367675abf62e1bf11c
Yashvardhan Singh <vs21yash@gmail.com>
2023-09-24 07:27:02 +0000

    rhomboid

--- client/src/Components/Map.jsx
+++ client/src/Components/Map.jsx
@@ -5,7 +5,6 @@ export default function Map() {
   return (
     <div className='relative col-span-2 row-span-1 bg-black text-white flex flex-col justify-between relative rounded-xl p-3 border-2 border-white'>
     <h1 className='font-pixel text-2xl'>Factory Layout</h1>
-    <img src={map} alt="" className='absolute inset-0 object-contain h-full w-full'/>
-</div>
+    </div>
   )
-}
+  }
--- client/src/Components/Rhomboid.jsx
+++ client/src/Components/Rhomboid.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import rhomboid from '../Assets/rhomboid.jpg'
 
 export default function Rhomboid () {
-    
+    return (
+    <div className='flex'>
+        <div className='flex-1 flex justify-center items-center relative'>
+          <img src={rhomboid} alt="rhomboid" />
+        </div>
+    </div>
+    )
 }
